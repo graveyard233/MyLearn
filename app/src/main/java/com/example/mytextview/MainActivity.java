1cfa3ec1a7df205c9837e6ef0c4c34841a4c9554
graveyard233 <382287750@qq.com>
2022-03-08 14:33:27 +0800

    搞定了progressbar，显示intent，还有toast学习

--- app/src/main/java/com/example/mytextview/MainActivity.java
+++ app/src/main/java/com/example/mytextview/MainActivity.java
@@ -4,19 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "lyd";
     private EditText et;
+    private  ProgressBar progressBar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+        //return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.page1_item:
+                Toast.makeText(this,"click page1",Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(this,MainActivity.class);
+                startActivity(intent1);
+                break;
+            case R.id.page2_item:
+                Toast.makeText(this,"click page2",Toast.LENGTH_SHORT).show();
+                Intent intent2 = new Intent(this,SecondActivity.class);
+                startActivity(intent2);
+                break;
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
         //Color.parseColor("#7fb80e")
 
         Button btn = findViewById(R.id.btn);
+
+        progressBar = findViewById(R.id.pb);
 
         //点击事件
         btn.setOnClickListener(new View.OnClickListener() {
@@ -64,5 +97,20 @@ public class MainActivity extends AppCompatActivity {
                 Log.e(TAG, "my password :" + text);
             }
         });
+    }
+
+
+    public void myclick(View view)
+    {
+        if (progressBar.getVisibility() == View.GONE)
+        {
+            progressBar.setVisibility(View.VISIBLE);
+            Toast.makeText(this,"显示进度条! short",Toast.LENGTH_SHORT).show();
+        }
+        else {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(this,"隐藏! long",Toast.LENGTH_LONG).show();
+        }
+
     }
 }
